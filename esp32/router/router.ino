66672d8194b83947fa87cfd95fc92194c865079a
Mathis Lécrivain <matlecr35@gmail.com>
2019-07-23 10:23:21 +0100

    add static IP to router
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/router/router.ino
+++ esp32/router/router.ino
@@ -3,19 +3,37 @@
 const char *ssid = "ESP32-Access-Point";
 const char *password = "123456789";
 
+// Static IP address
+const IPAddress local_IP(192, 168, 1, 1);
+// Gateway IP address
+const IPAddress gateway(192, 168, 1, 1);
+// subnet IP address
+const IPAddress subnet(255, 255, 0, 0);
+
 WiFiServer server(80);
 
 void setup()
 {
     Serial.begin(115200);
 
-    Serial.print("Setting AP (Access Point)…");
+    Serial.print("Setting AP (Access Point)… ");
+
+    if (!WiFi.softAPConfig(local_IP, gateway, subnet))
+    {
+        Serial.println("STA Failed to configure");
+    }
+    else
+    {
+        Serial.println("Success to configure STA :");
+        Serial.print("Local IP address   : ");
+        Serial.println(local_IP);
+        Serial.print("Gateway IP address : ");
+        Serial.println(gateway);
+        Serial.print("Subnet IP address  : ");
+        Serial.println(subnet);
+    }
 
     WiFi.softAP(ssid, password);
-
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
 
     server.begin();
 }
