a72e419ef7eb894868df0773529b61fb88cb8b62
Mathis Lécrivain <matlecr35@gmail.com>
2019-08-25 05:11:23 +0100

    add ftp server V1.0
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ python/FTPServer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user(
+    "test", "test", "/Users/username", perm="elradfmw")
+authorizer.add_anonymous("/Users/username", perm="elradfmw")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", 21), handler)
+server.serve_forever()
