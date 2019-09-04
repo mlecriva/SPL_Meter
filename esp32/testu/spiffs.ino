172ee5995358521f572efc3597e0485124df13ae
Mathis Lécrivain <matlecr35@gmail.com>
2019-09-04 16:56:04 +0100

    remove unused define
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/testu/spiffs.ino
+++ esp32/testu/spiffs.ino
@@ -1,10 +1,5 @@
 #include "FileManager.h"
 
-/* You only need to format SPIFFS the first time you run a
-    test or else use the SPIFFS plugin to create a partition
-    https://github.com/me-no-dev/arduino-esp32fs-plugin */
-#define FORMAT_SPIFFS_IF_FAILED true
-
 FileManager fm(SPIFFS);
 
 void setup()
