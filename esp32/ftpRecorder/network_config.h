e0660d8c6d20d411a6280828b4f07a12a7052490
Mathis Lécrivain <matlecr35@gmail.com>
2019-09-13 15:06:03 +0200

    network config
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ esp32/ftpRecorder/network_config.h
@@ -0,0 +1,25 @@
+#ifndef __NETWORK_CONFIG_H__
+#define __NETWORK_CONFIG_H__
+
+#include <WiFi.h>
+
+const char *ssid = "...";
+const char *password = "...";
+
+// Static IP address
+const IPAddress local_IP(192, 168, 1, 2);
+// Gateway IP address
+const IPAddress gateway(192, 168, 1, 1);
+// subnet IP address
+const IPAddress subnet(255, 255, 0, 0);
+
+// Broadcast
+const IPAddress udpServer_IP = (192, 168, 1, 255);
+// UDP port
+const int udpPort = 44444;
+
+char *ftp_server = "...";
+char *ftp_username = "...";
+char *ftp_password = "...";
+
+#endif /* __NETWORK_CONFIG_H__ */
